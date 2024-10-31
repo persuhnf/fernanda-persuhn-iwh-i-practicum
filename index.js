ef7ce845aa6fa20b8b617cc638abdda6b7748526
persuhnf <persuhnfernanda@gmail.com>
2024-10-31 18:57:20 -0400

    First commit to my Integrating With HubSpot I: Foundations practicum repository

--- index.js
+++ index.js
@@ -1,71 +1,62 @@
-const express = require('express');
-const axios = require('axios');
+require("dotenv").config({ path: "./pass.env" });
+const express = require("express");
+const axios = require("axios");
 const app = express();
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+app.get("/", async (req, res) => {
+  const bookUrl =
+    "https://api.hubapi.com/crm/v3/objects/books?properties=name,author_s_name,published_year";
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
 
-// * Code for Route 1 goes here
+  try {
+    const response = await axios.get(bookUrl, { headers });
+    const crmRecords = response.data.results;
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+    res.render("homepage", { title: "Homepage", crmRecords });
+  } catch (error) {
+    console.error("Error retrieving CRM records:", error);
+    res.status(500).send("An error occurred while retrieving CRM records.");
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+app.get("/update-cobj", (req, res) => {
+  res.render("updates", {
+    title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+  });
 });
-*/
 
+app.post("/update-cobj", async (req, res) => {
+  const newRecord = {
+    properties: {
+      name: req.body.name,
+      author_s_name: req.body.author_s_name,
+      published_year: req.body.published_year,
+    },
+  };
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+  const createUrl = `https://api.hubapi.com/crm/v3/objects/books`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    await axios.post(createUrl, newRecord, { headers });
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error creating a new CRM record:", error);
+    res.status(500).send("An error occurred while creating the record.");
+  }
+});
+
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
